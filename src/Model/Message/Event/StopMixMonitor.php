95a6c6fa45ea91e89ac9589e6a44261260410666
JohnHackworth <jhackworth2039@bonecloudops.com>
2023-04-27 13:11:57 +0300

    stop\start mix monitor events

---
+++ src/Model/Message/Event/StopMixMonitor.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @copyright 2020 ng-voice GmbH */
+
+declare(strict_types=1);
+
+namespace Mytryk\AriClient\Model\Message\Event;
+
+/**
+ * Event showing failure of a recording operation.
+ *
+ * @package Mytryk\AriClient\Model\Message\Event
+ *
+ * @author Lukas Stermann <lukas@ng-voice.com>
+ */
+class StopMixMonitor extends Event
+{
+
+}
